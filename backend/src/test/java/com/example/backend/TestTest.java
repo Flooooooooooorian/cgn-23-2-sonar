7264c02afcef0ffc33e7897d28f64232e2f68103
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-06-28 14:40:37 +0200

    Add jacoco

---
+++ backend/src/test/java/com/example/backend/TestTest.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @org.junit.jupiter.api.Test
+    void testTest() {
+        Test t =  new Test();
+        assertEquals(t.getHello(), "Hello");
+    }
+
+}
